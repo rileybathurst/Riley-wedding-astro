81c9abe15416480b2070feb3af703f54562bdebb
Riley <riley@rileybathurst.com>
2025-06-24 21:43:59 -0700

    video

--- src/types/couple-card-types.ts
+++ src/types/couple-card-types.ts
@@ -1,10 +1,11 @@
 export type CoupleCardTypes = {
   id: string; // I think theres a better version of this
-  bride: string;
-  groom: string;
+  bride?: string;
+  groom?: string;
   slug: string;
   excerpt: string;
-  image: {
+  title?: string;
+  hero: {
     url: string;
     alternativeText: string;
     width: number;
